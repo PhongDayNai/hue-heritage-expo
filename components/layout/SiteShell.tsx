a6a6bdc77089100fd7378cacc4c38eb9e5421490
PhongDayNai <dev.dhpho266@gmail.com>
2026-03-29 15:18:45 +0000

    fix(splash): remove overlay that could block page interactions

--- components/layout/SiteShell.tsx
+++ components/layout/SiteShell.tsx
@@ -1,11 +1,9 @@
 import Footer from './Footer';
 import Topbar from './Topbar';
-import GlobalSplash from '../ui/GlobalSplash';
 
 export default function SiteShell({ children }: { children: React.ReactNode }) {
   return (
     <main>
-      <GlobalSplash />
       <Topbar />
       {children}
       <Footer />
